a1bb6a717dade2d14a18a5b87a477babd6bfb836
Piotr Kumorek <piotr.kumi@gmail.com>
2019-03-30 10:38:10 +0100

    refactor: add color consts

---
+++ src/consts.js
@@ -0,0 +1,6 @@
+export const BLUE = "#3978ff";
+export const RED = "#fb1f43";
+export const ORANGE = "#ffa200";
+export const VIOLET = "#db54ff";
+
+export const colors = [RED, ORANGE, VIOLET, BLUE];
